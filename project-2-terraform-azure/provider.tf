495786f5be3a568aee8f731a3810b22fb729e196
Atefor Lesley Nkezi <lesleynkezi@gmail.com>
2026-01-18 14:46:34 +0530

    Added Terraform GitHub Actions CI/CD pipeline

--- project-2-terraform-azure/provider.tf
+++ project-2-terraform-azure/provider.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+     resource_group_name  = "rg-terraform-backend"
+     storage_account_name = "terraformstatelesley"
+     container_name       = "tfstate"
+     key                  = "project2terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
